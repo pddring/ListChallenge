0c8129d63cf149c7bd8913e20da5cd3af10eb0f9
Adam Boyes <16BoyesA@fulford.york.sch.uk>
2022-01-10 13:31:48 +0000

    All issues resolved

--- ListChallenge/Program.cs
+++ ListChallenge/Program.cs
@@ -10,10 +10,16 @@ namespace ListChallenge
     {
         static void Main(string[] args)
         {
-            List<string> names = new List<string> { "Will 1", "Will 2", "Ethan"};
-            names.Add("Tom");
-
-            Console.WriteLine(names[3]);
+            Random rnd = new Random();
+            
+            List<string> names = new List<string> { "Will 1", "Will 2", "Ethan", "Adam", "Cormac", "Tom", "Ben", "Henry", "Mr. Dring", "Ashton", "Ethan"};
+            List<int> score = new List<int> {  };
+            for (int i = 0; i < names.Count(); i++)
+            {
+                score.Add(rnd.Next(100));
+                Console.WriteLine(names[i] +" "+score[i]);
+            }
+            Console.ReadLine();
         }
     }
 }

f2ec4597e0a559179cdb3863983069b4a50bade5
Adam Boyes <16BoyesA@fulford.york.sch.uk>
2022-01-10 13:34:08 +0000

    Close #1, Close #2, Close #3

--- ListChallenge/Program.cs
+++ ListChallenge/Program.cs
@@ -6,20 +6,28 @@ using System.Threading.Tasks;
 
 namespace ListChallenge
 {
+
     class Program
     {
+
         static void Main(string[] args)
         {
+
             Random rnd = new Random();
             
             List<string> names = new List<string> { "Will 1", "Will 2", "Ethan", "Adam", "Cormac", "Tom", "Ben", "Henry", "Mr. Dring", "Ashton", "Ethan"};
             List<int> score = new List<int> {  };
+
             for (int i = 0; i < names.Count(); i++)
             {
+
                 score.Add(rnd.Next(100));
                 Console.WriteLine(names[i] +" "+score[i]);
+
             }
+
             Console.ReadLine();
+
         }
     }
 }
